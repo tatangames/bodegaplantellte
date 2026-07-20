bd90da82f9a86ce12469a2ba513dda90cba960c9
Tatan <tatangamess@gmail.com>
2026-07-20 14:15:07 -0600

    agregando reportes

--- routes/web.php
+++ routes/web.php
@@ -152,7 +152,7 @@ Route::middleware('auth:admin')->group(function () {
     Route::post('/admin/informacion/actualizar/px', [ReportesController::class, 'actualizarPxInformacionGeneral'])
         ->name('admin.informacion.actualizar.px');
 
-
+    
 
 
 
